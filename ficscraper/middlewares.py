31925364c859190f3832f4892f23da224918be3d
AlandisAyupov <100100917+AlandisAyupov@users.noreply.github.com>
2023-07-23 10:31:06 +0200

    Update middlewares.py

--- ficscraper/middlewares.py
+++ ficscraper/middlewares.py
@@ -3,13 +3,15 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import json
+from re import I
 from scrapy import signals
 
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
 
 
-class FicscraperSpiderMiddleware:
+class ScraperSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -56,7 +58,7 @@ class FicscraperSpiderMiddleware:
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class FicscraperDownloaderMiddleware:
+class ScraperDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
@@ -101,3 +103,4 @@ class FicscraperDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
